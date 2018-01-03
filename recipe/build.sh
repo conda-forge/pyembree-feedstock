6208cf150d97a61315506b915b0fb4e9685f74be
Anthony Scopatz <scopatz@gmail.com>
2018-01-03 12:07:24 -0500

    resolved conflicts

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,4 @@ set -x
 export C_INCLUDE_PATH="${PREFIX}/include:${C_INCLUDE_PATH}"
 export CPLUS_INCLUDE_PATH="${PREFIX}/include:${CPLUS_INCLUDE_PATH}"
 
-python setup.py install --prefix=$PREFIX
+${PYTHON} setup.py install --prefix=$PREFIX
