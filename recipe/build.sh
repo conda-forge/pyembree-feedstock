84872970f86117e2b33c4d554d5d4e5bb5d2f586
Daniel Suess <daniel.suess@nearmap.com>
2022-12-14 16:47:08 +1100

    Trying to fix build with pip

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,4 +6,4 @@ export C_INCLUDE_PATH="${PREFIX}/include:${C_INCLUDE_PATH}"
 export CPLUS_INCLUDE_PATH="${PREFIX}/include:${CPLUS_INCLUDE_PATH}"
 
 # ${PYTHON} setup.py install --prefix=$PREFIX
-${PYTHON} -m pip install -vv --no-deps --ignore-installed --target ${PREFIX} .
+${PYTHON} -m pip install -vv --no-deps --ignore-installed .
